e1ba58625c469dfa58421822948eea0687710b70
Lora Revelo Bryan Paúl <bryan.lora@epn.edu.ec>
2020-07-03 16:06:00 -0500

    Clase 03-07-2020 Envio de parametros

--- 01-http/introduccion/src/app.module.ts
+++ 01-http/introduccion/src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {HttpJuegoModule} from "./http/http-juego.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        //aquí van otros modulos
+        HttpJuegoModule
+    ],
+    controllers: [
+        //aqui van controladores de app modulet
+
+        AppController],
+    providers: [
+        //aqui van servicios de app module
+        AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
